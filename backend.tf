c6716a66fe1a43d013e2871c9f9c7dd85e604ee0
EfczyEn <125197287+EfczyEn@users.noreply.github.com>
2023-04-11 14:28:07 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.11"
   backend "gcs" {
-    bucket = "dareit-bucket-tf-ci-sw"
+    bucket = "dareit-bucket-ci-sw"
     prefix = "dev"
   }
   required_providers {
