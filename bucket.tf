162a781ef276bab70ee936289e071953303b89b8
EfczyEn <125197287+EfczyEn@users.noreply.github.com>
2023-04-11 16:14:00 +0200

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -5,6 +5,23 @@ resource "google_storage_bucket" "dareit-bucket-ci-sw" {
  storage_class = "STANDARD"
 
 uniform_bucket_level_access = false
+
+force_destroy = true
+  website {
+  main_page_suffix = "index.html"
+  }
+  cors {
+    origin          = ["*"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+}
+}
+
+resource "google_storage_bucket_object" "dareit-bucket-ci-sw-src" {
+  name   = "index.html"
+  source = "public/index.html"
+  bucket = google_storage_bucket.dareit-bucket-ci-sw.name
 }
 
 resource "google_storage_bucket_access_control" "public_rule" {
