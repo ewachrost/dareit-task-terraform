394cde37419e9f8d9aea813f6d201689c7340338
EfczyEn <125197287+EfczyEn@users.noreply.github.com>
2023-04-12 19:54:30 +0200

    fix: vm and bucket repaired

---
+++ main.tf
@@ -0,0 +1,56 @@
+resource "google_compute_instance" "dareit-vm-ci-sw-1" {
+  name         = "dareit-vm-tf-ci-sw-1"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["dareit"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        managed_by_terraform = "true"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
+resource "google_storage_bucket" "dareit-bucket-ci-sw-1" {
+ project       = "potent-bloom-377613"
+ name          = "dareit-bucket-tf-ci-sw-1"
+ location      = "US"
+ storage_class = "STANDARD"
+
+uniform_bucket_level_access = false
+
+force_destroy = true
+  website {
+  main_page_suffix = "index.html"
+  }
+  cors {
+    origin          = ["*"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+}
+}
+
+resource "google_storage_bucket_object" "dareit-bucket-ci-sw-src-1" {
+  name   = "index.html"
+  source = "public/index.html"
+  bucket = google_storage_bucket.dareit-bucket-ci-sw-1.name
+}
+
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.dareit-bucket-ci-sw-1.id
+  role   = "READER"
+  entity = "allUsers"
+
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = "potent-bloom-377613"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
